e9236ded8ee1974fb1de8a68798cdf3c64b1351b
jonasjaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-12-27 15:15:06 +0100

    port *stop

--- TomokoBot/TomokoBot.js
+++ TomokoBot/TomokoBot.js
@@ -674,41 +674,6 @@ playCmd.registerSubcommandAlias("listen", "listenmoe");
 playCmd.registerSubcommandAlias("lmoe", "listenmoe");
 playCmd.registerSubcommandAlias("listen.moe", "listenmoe");
 
-bot.registerCommand("stop", (message, args) => { // Command to stop playing and leave the channel
-    if (args.length === 0) {
-        if (musicGuilds.has(message.member.guild.id)) {
-            var guild = musicGuilds.get(message.member.guild.id);
-            guild.queue = [];
-            if(guild.connection.playing) { // Stop playing if the connection is playing something
-                guild.connection.stopPlaying();
-            }
-            bot.leaveVoiceChannel(guild.channelID);
-        } else {
-            bot.createMessage(message.channel.id, {
-                                        "embed": {
-                                            "title": "Tomoko's Music Player",
-                                            "description": "I'm n-not in y-your voice channel!\nP-Please use `join`!",
-                                            "color": 16684873,
-                                            "thumbnail": {
-                                                "url": bot.user.avatarURL
-                                            },
-                                            "author": {
-                                                "name": "Tomoko Bot",
-                                                "icon_url": bot.user.avatarURL
-                                            }
-                                        }
-                                    });
-        }
-    } else {
-        invalidArgs(message, message.author, message.content.split(" ")[0]);
-    }
-},
-{
-    "cooldown": 4000,
-    "cooldownMessage": messages.cooldown,
-    "cooldownReturns": 4
-});
-
 bot.registerCommand("skip", (message, args) => { // Command to skip the current song
     if (args.length === 0) {
         if (message.member.permission.has("manageChannels")) {
---
+++ TomokoBot/commands/stop.js
@@ -0,0 +1,35 @@
+module.exports.run = (message, args) => { // Command to stop playing and leave the channel
+    if (args.length === 0) {
+        if (musicGuilds.has(message.member.guild.id)) {
+            var guild = musicGuilds.get(message.member.guild.id);
+            guild.queue = [];
+            if(guild.connection.playing) { // Stop playing if the connection is playing something
+                guild.connection.stopPlaying();
+            }
+            bot.leaveVoiceChannel(guild.channelID);
+        } else {
+            bot.createMessage(message.channel.id, {
+                                        "embed": {
+                                            "title": "Tomoko's Music Player",
+                                            "description": "I'm n-not in y-your voice channel!\nP-Please use `join`!",
+                                            "color": 16684873,
+                                            "thumbnail": {
+                                                "url": bot.user.avatarURL
+                                            },
+                                            "author": {
+                                                "name": "Tomoko Bot",
+                                                "icon_url": bot.user.avatarURL
+                                            }
+                                        }
+                                    });
+        }
+    } else {
+        invalidArgs(message, message.author, message.content.split(" ")[0]);
+    }
+};
+
+module.exports.options = {
+    "cooldown": 4000,
+    "cooldownMessage": messages.cooldown,
+    "cooldownReturns": 4
+};
